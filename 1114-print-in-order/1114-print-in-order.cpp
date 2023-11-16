b5b174f8d1d74e9ee6e547ba67623617fa5dbde6
Akash Singh <akashsingh03031@gmail.com>
2023-11-16 18:35:44 +0530

    Time: 14 ms (30.45%), Space: 7.5 MB (78.21%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,22 @@
+class Foo {
+    binary_semaphore s1{1},s2{0},s3{0};
+public:
+    Foo() {}
+
+    void first(function<void()> printFirst) {
+        s1.acquire();
+        printFirst();
+        s2.release();
+    }
+
+    void second(function<void()> printSecond) {
+        s2.acquire();
+        printSecond();
+        s3.release();
+    }
+
+    void third(function<void()> printThird) {
+        s3.acquire();
+        printThird();
+    }
+};
